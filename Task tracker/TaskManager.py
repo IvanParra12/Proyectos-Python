bb9f26d96f5c2bfcbe450312faf5ff7759153aa0
Iván Parra <ivandarioparraiglesias@gmail.com>
2024-12-04 20:21:44 +0100

    Función para cargar las tareas del archivo JSON

--- Task tracker/TaskManager.py
+++ Task tracker/TaskManager.py
@@ -1,10 +1,36 @@
+import json
+import os
 from Task import Task
 
 class TaskManager():
     
-    def __init__(self) -> None:
-        pass
+    def __init__(self, ficheroTareas = "tareas.json"):
+        self.__ficheroTareas = ficheroTareas
+        self.__tareas= []
+        self.cargar_tareas()
+        
+    def get_ficheroTareas(self):
+        return self.__ficheroTareas
     
+    def get_tareas(self):
+        return self.__tareas
+        
+    def cargar_tareas(self):
+        # Carga las tareas desde el archivo JSON. Si no existe, crea uno vacío
+        if not os.path.exists(self.get_ficheroTareas()):
+            with open(self.get_ficheroTareas(), 'w') as archivo:
+                json.dump([], archivo)
+            self.get_tareas() = []
+            print(f'Archivo {self.get_ficheroTareas()} creado.')
+        else:
+            with open(self.get_ficheroTareas(), 'r') as archivo:
+                try:
+                    datos_tareas = json.load(archivo)
+                    self.get_tareas() = [Task.from_dict(tarea) for tarea in datos_tareas]
+                except json.JSONDecodeError:
+                    self.get_tareas() = []
+                    print(f'Archivo {self.get_ficheroTareas()} vacío o con formato inválido. Inicializando una lista vacía.')
+      
     def update_status(self, command, id):
         
         # Obtenemos la tarea por ID
